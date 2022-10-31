fec827a30992057cfda01f2690b2cb4598285ef2
zxdfg114 <zxdfg114@gmail.com>
2022-11-01 01:54:37 +0900

    format : partial

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <address>email : zxdfg114@gmail.com</address>
+      <small>&copy; 2022 유기백 비상업용 포트폴리오</small>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <section>
+        <h1>FAKE</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis magni
+          consequatur a unde consectetur, non similique soluta dolores sed quam
+          atque vel reprehenderit quia aut! Explicabo fuga porro architecto
+          expedita? Cupiditate magni eos recusandae maiores adipisci atque
+          eligendi vero tempore quisquam labore suscipit cupiditate!
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default Hero;
